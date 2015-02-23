4e60f02f4691ab5c433c2d3573cc9bce1819c228
Alexander Biles <alexander.c.biles@gmail.com>
2015-02-23 15:30:46 -0500

    set up devise

--- veezing/config/routes.rb
+++ veezing/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  root to: "videos#index"
+  devise_for :users
+  root to: "users#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
