b6ce1624c7c42a8d7db3beaa84f8c5cf077df578
Alexander Biles <alexander.c.biles@gmail.com>
2015-02-24 15:12:03 -0500

    added next and prev button functionality started to set up comments and zings controllers and routes

--- veezing/app/controllers/videos_controller.rb
+++ veezing/app/controllers/videos_controller.rb
@@ -1,6 +1,21 @@
+# class VideosController < ApplicationController
+# 	def show
+# 		@videos = Video.all
+# 		puts @videos
+# 	end
+# end
+
 class VideosController < ApplicationController
-	def show
-		# need to write a method to pull the first video id
-		@videos = Video.all
-	end
+  def index
+  	@video = Video.first
+  	redirect_to @video
+  end
+
+  def show
+    @video = Video.find(params[:id])
+    @video_last = Video.first
+    @video_first = Video.last
+  end
+  
+
 end
--- veezing/app/models/video.rb
+++ veezing/app/models/video.rb
@@ -1,4 +1,18 @@
 class Video < ActiveRecord::Base
 	has_many :zings
 	has_many :comments
+	default_scope -> { order('created_at DESC') }
+
+	def prev
+		if id > 1
+		Video.find(id - 1)
+		end
+	end
+
+  	def next
+  		if id < Video.first.id
+    	Video.find(id + 1)
+    	end
+  	end
 end
+
