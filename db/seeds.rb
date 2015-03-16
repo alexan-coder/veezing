fc4306713863f4e40678e22a7800385e07677edd
Alexander Biles <alexander.c.biles@gmail.com>
2015-03-16 17:58:16 -0400

    re-added site files

---
+++ db/seeds.rb
@@ -0,0 +1,22 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+Video.create([
+  	{ :title => "ll Capo by Yuri Ancarani", :description => "Italian filmmaker, Yuri Ancaran, captures the otherworldly landscape of Carrara's marble quarries in the Apuan Alps, Northwest Italy.", :url => "du9_Kn2y2VA" },
+ 	{ :title => "Outer Space Timelapse", :description => "Beautiful timelapse from the International Space Station. ", :url => "ONaPq2L-MRg" },
+  	{ :title => "Owl Laugh", :description => "This video is such a hoot!", :url => "M5p9JO9JgvU" },
+   	{ :title => "Fresh Guacamole", :description => "Beautifully crafted stop motion short.", :url => "dNJdJIwCF_Y" },
+  	{ :title => "Dat Whistle", :description => "A skill you never knew you wanted.", :url => "xaPoBhiMkgs" },
+	{ :title => "Bane Cat", :description => "A combo of cats and batman. This video wins the internet.", :url => "5ywjpbThDpE" },
+   	{ :title => "Super slow motion jiggly stuff", :description => "Who knew watching things jiggle in slow mo would be so fun?", :url => "alXL3klRKBE" },
+])
+
+# Zing.create(user_id: 1, video_id: 2)
+
